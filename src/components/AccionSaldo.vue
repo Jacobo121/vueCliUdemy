e1b28240ab36dbd989470521796dfc9223ff2dc9
Jacobo Arcila <wagner.arcila.jacobo@gmail.com>
2021-07-10 18:15:04 -0500

    Comunicacion entre componentes

---
+++ src/components/AccionSaldo.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <button> {{texto}} </button>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        texto: String,
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -6,17 +6,33 @@
         <div v-for="(item, index) in servicios" :key="index">
             {{index + 1 }} - {{ item }}
         </div>
+        <AccionSaldo @click="disminuir" texto="Disminuir Saldo" />
+        <AccionSaldo @click="aumentar" texto="Aumentar Saldo" />
     </div>
 </template>
 <script>
+import AccionSaldo from './AccionSaldo.vue'
 
 export default {
+    name: "Cuenta",
+    components: {
+        AccionSaldo
+    },
     data() {
         return {
             saldo: 1000,
             cuenta: 'visa',
             estado: true,
             servicios: ['giro', 'abono', 'transferencia']
+        }
+    },
+
+    methods: {
+        aumentar() {
+            this.saldo = this.saldo + 100
+        },
+        disminuir() {
+            this.saldo = this.saldo - 100
         }
     },
 }
