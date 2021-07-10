0c1e10246b3ce7a93d6ae20430fd2bb1d71cc7c2
Jacobo Arcila <wagner.arcila.jacobo@gmail.com>
2021-07-09 22:17:10 -0500

    Creando Componentes

---
+++ src/components/Cuenta.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div>
+        <h2>Tipo de cuenta: {{cuenta}} </h2>
+        <h2>Saldo: {{saldo}}  </h2>
+        <h2>Cuenta {{estado ? 'Activa' : 'Desactivada' }} </h2>
+        <div v-for="(item, index) in servicios" :key="index">
+            {{index + 1 }} - {{ item }}
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            saldo: 1000,
+            cuenta: 'visa',
+            estado: true,
+            servicios: ['giro', 'abono', 'transferencia']
+        }
+    },
+}
+
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/Titulo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{texto}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Titulo',
+    props: {
+        texto: String,
+    }
+}
+
+</script>
+
+<style scoped>
+    /*scoped hace que los estilos que pongamos aqui, solo afecten al componente  */
+    h1 {
+        color: peru;
+    }
+</style>
